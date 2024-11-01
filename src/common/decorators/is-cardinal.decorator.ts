37eb42255c845d4ee0dab27a72895ba8a65a319c
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-01 14:54:50 +0300

    adding decorators

---
+++ src/common/decorators/is-cardinal.decorator.ts
@@ -0,0 +1,10 @@
+import { applyDecorators } from '@nestjs/common';
+import { IsInt, IsPositive, ValidationOptions } from 'class-validator';
+
+/**
+ * Checks if the value is a positive integer greater than zero.
+ */
+
+export const IsCardinal = (
+  validationOption?: ValidationOptions,
+): PropertyDecorator => applyDecorators(IsInt(validationOption), IsPositive(validationOption));
