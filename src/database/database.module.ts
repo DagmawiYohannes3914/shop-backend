0324b446c6facbf824602d19dce3deb0f9ac094e
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-04 16:39:57 +0300

    reducing boiler plates

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigType } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
-    ConfigModule.forFeature(databaseConfig),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(databaseConfig)],
-      inject: [databaseConfig.KEY],
-      useFactory: (databaseConfiguration: ConfigType<typeof databaseConfig>,) => ({
-        type: 'postgres',
-        url: databaseConfiguration.url,
-        autoLoadEntities: true,
-      }),
-    }),
+    ConfigModule.forFeature(databaseConfig), // Partial registration
+    TypeOrmModule.forRootAsync(databaseConfig.asProvider()), // Simplified setup
   ],
 })
 export class DatabaseModule {}
