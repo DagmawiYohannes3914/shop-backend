922e73f02044e9c496bf606623adb948840b674e
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-05 17:10:32 +0300

    order entity

--- src/domain/orders/entities/order.entity.ts
+++ src/domain/orders/entities/order.entity.ts
@@ -1,1 +1,17 @@
-export class Order {}
+
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { RegistryDates } from "common/embedded/registry-dates.embedded";
+import { OrderStatus } from "orders/enums/order-status.enum";
+@Entity()
+export class Order {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  status: OrderStatus;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
+
+}
