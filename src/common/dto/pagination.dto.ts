dc07174f5799a255008e27cda6646785ebbc82b4
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-07 12:45:23 +0300

    pagination dto update

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,15 +1,25 @@
-import { IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsOptional, Min } from 'class-validator';
 import { IsCardinal } from 'common/decorators/is-cardinal.decorator';
 
 export class PaginationDto {
+
+
+  @IsOptional()
+  @Type(() => Number) // Transform the query parameter to a number
+  @IsCardinal()
+  page?: number = 1; // Default to the first page if not provided
+
+  
+  @Type(() => Number)
+  @Min(1)
   @IsOptional()
   @IsCardinal()
-  readonly limit: number;
+  readonly limit?: number = 5;
 
 
   @IsOptional()
   @IsCardinal()
   readonly offset: number;
-
 
 }
