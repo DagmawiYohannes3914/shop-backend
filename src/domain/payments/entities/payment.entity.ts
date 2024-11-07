188280e869d2ab90a8e4dc1e99244cc6a022c76f
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-07 12:45:39 +0300

    payment entity update

--- src/domain/payments/entities/payment.entity.ts
+++ src/domain/payments/entities/payment.entity.ts
@@ -1,1 +1,18 @@
-export class Payment {}
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, OneToOne, JoinColumn } from "typeorm";
+import { RegistryDates } from "common/embedded/registry-dates.embedded";
+import { Order } from "orders/entities/order.entity";
+@Entity()
+export class Payment {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
+
+  @OneToOne(() => Order, (order) => order.payment, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn()
+  order: Order
+}
