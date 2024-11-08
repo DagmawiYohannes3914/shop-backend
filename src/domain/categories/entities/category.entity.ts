603f7e241b1313a89c83b15a52e4e1017502b25b
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-08 11:50:58 +0300

    category entity

--- src/domain/categories/entities/category.entity.ts
+++ src/domain/categories/entities/category.entity.ts
@@ -1,1 +1,14 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, OneToOne, JoinColumn } from "typeorm";
+import { RegistryDates } from "common/embedded/registry-dates.embedded";
+
+
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
+}
