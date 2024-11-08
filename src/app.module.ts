f50239bbd3a5ea0db8df2fa091a8d72ceac49ffd
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-08 08:41:08 +0300

    catagories setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { EnvModule } from './env/env.module';
 import { OrdersModule } from './domain/orders/orders.module';
 import { Payments~Module } from './domain/payments~/payments~.module';
 import { PaymentsModule } from './domain/payments/payments.module';
+import { CategoriesModule } from './domain/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, CommonModule, DatabaseModule, EnvModule, OrdersModule, Payments~Module, PaymentsModule],
+  imports: [UsersModule, CommonModule, DatabaseModule, EnvModule, OrdersModule, Payments~Module, PaymentsModule, CategoriesModule],
   controllers: [],
   providers: [],
 })
