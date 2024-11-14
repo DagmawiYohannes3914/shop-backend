b8fe15a360e46ea1aebe361700c3011884993723
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-14 12:30:37 +0300

    product logic

--- src/domain/products/dto/create-product.dto.ts
+++ src/domain/products/dto/create-product.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProductDto {}
+import { IsNumber, IsOptional, IsPositive, Length } from "class-validator";
+
+export class CreateProductDto {
+  @Length(2, 50)
+  readonly name: string;
+
+  @IsOptional()
+  @Length(1, 500)
+  description: string;
+
+  @IsNumber({maxDecimalPlaces: 2})
+  @IsPositive()
+  price: number;
+
+  categories: number[];
+
+}
