cf9d76441ede477ea3a306f123147351715199a9
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-15 15:34:18 +0300

    order logic implementation

--- src/domain/orders/orders.module.ts
+++ src/domain/orders/orders.module.ts
@@ -4,10 +4,11 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
+import { Product } from 'products/entities/product.entity';
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Product])],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- src/domain/orders/orders.service.ts
+++ src/domain/orders/orders.service.ts
@@ -3,9 +3,11 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Order } from './entities/order.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { PaginationDto } from 'common/dto/pagination.dto';
 import { Default_Page_Size } from 'common/util/common.constants';
+import { Product } from 'products/entities/product.entity';
+import { OrderItem } from './entities/order-item.entity';
+import { OrderItemDto } from './dto/order-item.dto';
 
 @Injectable()
 export class OrdersService {
@@ -13,12 +15,23 @@ export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private readonly ordersRepository: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly ordersItemRepository: Repository<OrderItem>,
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
   ){}
+  
 
- create(createOrderDto: CreateOrderDto) {
-    // const {items} = createOrderDto;
+ async create(createOrderDto: CreateOrderDto) {
+
+  const { items } = createOrderDto;
+  const itemsWithPrice = await Promise.all(
+    items.map(item => this.createOrderItemWithPrice(item)),
+  ) 
+
     const order = this.ordersRepository.create({
       ...createOrderDto,
+      items: itemsWithPrice,
     });
 
     return this.ordersRepository.save(order);
@@ -38,7 +51,11 @@ export class OrdersService {
     const order = await this.ordersRepository.findOne({
       where: { id },
       relations: {
+        items: {
+          product: true,
+        },
         customer: true,
+        payment: true,
       },
     });
     if (!order) {
@@ -47,12 +64,26 @@ export class OrdersService {
     return order;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
 
   async remove(id: number) {
     const user = await this.findOne(id);
     return this.ordersRepository.remove(user);
   }
+
+  private async createOrderItemWithPrice(orderItemDto: OrderItemDto){
+    const { id } = orderItemDto.product;
+
+    const product = await this.productsRepository.findOneBy({id});
+
+    const { price } = product;
+
+    const orderItem = this.ordersItemRepository.create({
+      ...orderItemDto,
+      price,
+    });
+
+    return orderItem;
+
+  }
+  
 }
