ab51cf7327832a1b23f2ee969d419ed13ce94eb2
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-21 16:19:17 +0300

    seeding

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SeedingModule } from './seeding/seeding.module';
 import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forFeature(databaseConfig), // Partial registration
-    TypeOrmModule.forRootAsync(databaseConfig.asProvider()), // Simplified setup
+    TypeOrmModule.forRootAsync(databaseConfig.asProvider()), SeedingModule, // Simplified setup
   ],
 })
 export class DatabaseModule {}
