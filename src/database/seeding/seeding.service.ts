d0dd7e13c60f4e05b71d5de7e0714b080f9bfa48
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-11-22 15:04:01 +0300

    seeding

--- src/database/seeding/seeding.service.ts
+++ src/database/seeding/seeding.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class SeedingService {}
+export class SeedingService {
+  constructor(private readonly dataSource: DataSource){}
+  async seed() {
+    const queryRunner = this.dataSource.createQueryRunner()
+  }
+}
