c30f05867caa648c31955272f6904646c91c41f3
dagmawiyohannes3914 <dagmawiyohannes323@gmail.com>
2024-12-02 10:05:59 +0300

    adding helmet

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { VALIDATION_PIPE_OPTIONS } from './common/util/common.constants';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalPipes(new ValidationPipe(VALIDATION_PIPE_OPTIONS));
   app.enableCors();
+  app.use(helmet());
+
+
   await app.listen(process.env.PORT ?? 3000);
-}
+} 
 bootstrap();
